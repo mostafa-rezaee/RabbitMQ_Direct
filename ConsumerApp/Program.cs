d90c9c73e1e8c75cecc0c8daf78231e33a00d5f8
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-12-09 17:05:22 +0330

    Added consumer reading capability.

--- ConsumerApp/Program.cs
+++ ConsumerApp/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+string queueName = "testQueue1";
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+IConnection connection = await connectionFactory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (sender, args) => { 
+    var body = args.Body.ToArray();
+    var result = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Your number is: " + result);
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+Console.ReadLine();
+
--- ProducerApp/Program.cs
+++ ProducerApp/Program.cs
@@ -1,5 +1,23 @@
 ﻿
+using RabbitMQ.Client;
+using System.Text;
+
 Console.WriteLine("Hello, Please enter your phone number:");
-Console.ReadLine();
+var message = Console.ReadLine();
+string queueName = "testQueue1";
+
+var connectionFactory = new ConnectionFactory() { 
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+IConnection connection = await connectionFactory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
 
 
+if (message != null)
+    await channel.BasicPublishAsync(exchange: String.Empty, routingKey: queueName, body: Encoding.UTF8.GetBytes(message));
+
+Console.WriteLine(message + " sent to queue.");
+Console.ReadKey();
